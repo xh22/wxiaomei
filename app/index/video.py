16b18cbc7df138e96da85dcfc926b8d3062c9ca3
xh22 <910192724@qq.com>
2016-06-20 23:10:08 +0800

    faga

--- app/index/video.py
+++ app/index/video.py
@@ -16,7 +16,7 @@ class VideoCamera(object):
         #self.video = cv2.VideoCapture(0)
         # If you decide to use video.mp4, you must have this file in the folder
         # as the main.py.
-        self.video = cv2.VideoCapture('video{}.mp4'.format(index))
+        self.video = cv2.VideoCapture('static/video/video{}.mp4'.format(index))
     
     def __del__(self):
         self.video.release()
