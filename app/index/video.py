d15c01fd7957c83235e8c189b7010d84199661f2
xh22 <910192724@qq.com>
2016-06-21 12:19:59 +0800

    cached video

--- app/index/video.py
+++ app/index/video.py
@@ -5,42 +5,19 @@ from flask import session, render_template, request, redirect, Response
 from flask.views import MethodView
 
 from main import app, db
-
-import cv2
-
-class VideoCamera(object):
-    def __init__(self, index):
-        # Using OpenCV to capture from device 0. If you have trouble capturing
-        # from a webcam, comment the line below out and use a video file
-        # instead.
-        #self.video = cv2.VideoCapture(0)
-        # If you decide to use video.mp4, you must have this file in the folder
-        # as the main.py.
-        self.video = cv2.VideoCapture('main/static/video/video{}.mp4'.format(index))
-    
-    def __del__(self):
-        self.video.release()
-    
-    def get_frame(self):
-        success, image = self.video.read()
-        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-        # so we must encode it into JPEG in order to correctly display the
-        # video stream.
-        ret, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tostring()
+from unity import cached_video
 
 @app.route('/news')
 def news():
     return render_template('news.html')
 
 def gen(camera):
-    while True:
-        frame = camera.get_frame()
+    for frame in camera.get_frame():
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 @app.route('/video_feed/<int:index>')
 def video_feed(index):
-    return Response(gen(VideoCamera(index)),
+    return Response(gen(cached_video.VideoCamera(index)),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
---
+++ app/unity/cached_video.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import cv2
+from . import cache
+
+class VideoCamera(object):
+    __metaclass__ = cache.Cached
+    def __init__(self, video):
+        # Using OpenCV to capture from device 0. If you have trouble capturing
+        # from a webcam, comment the line below out and use a video file
+        # instead.
+        #self.video = cv2.VideoCapture(0)
+        # If you decide to use video.mp4, you must have this file in the folder
+        # as the main.py.
+        self.seed = cv2.VideoCapture('main/static/video/video{}.mp4'.format(index))
+        self.video = []
+        while True:
+            success, image = self.video.read()
+            if success:
+                self.video.append(image)
+            else:
+                break
+            
+    
+    def __del__(self):
+        del(self.video)
+        self.seed.release()
+    
+    def get_frame(self):
+        for image in self.video:
+            ret, jpeg = cv2.imencode('.jpg', image)
+            yield jpeg.tostring()
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
