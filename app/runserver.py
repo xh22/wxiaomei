e277eb6cd509266965c569398dde0ccd092d0c32
xh22 <910192724@qq.com2>
2016-06-23 11:41:57 +0800

    faga

--- app/runserver.py
+++ app/runserver.py
@@ -1,9 +1,15 @@
 import index
 from main import app as application 
 from datetime import timedelta
+from flask.ext.session import Session
 
 app = application
-app.permanent_session_lifetime = timedelta(minutes=60)
+SESSION_TYPE = 'redis'
+PERMANENT_SESSION_LIFETIME = timedelta(minutes=60)
+app.config.from_object(__name__)
+Session(app)
+
+#app.permanent_session_lifetime = timedelta(minutes=60)
 
 if __name__ == '__main__':
     app.run(host="120.25.171.88", port=8000, debug=True, use_reloader=True)
