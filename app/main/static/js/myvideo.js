7445fa2f29c6f94d6b9d63b56ac88891e0bb8e49
xh22 <910192724@qq.com>
2016-06-23 22:00:59 +0800

    faga

--- app/main/static/js/myvideo.js
+++ app/main/static/js/myvideo.js
@@ -1,11 +1,12 @@
-<script type="text/javascript">
+$(document).ready(function() {
     jwplayer("container").setup({  
         flashplayer: "/static/plugin/player.flash.swf",  
         file: "/static/video/video1.mp4",  
-        height: 622,  
-        width:1104,  
+        image:"static/img/full/26.jpg",
+        height: 197,  
+        width:350,  
         stretching : 'fill',  
         streamer:"start",  
         provider:"http",  
     });  
-</script>
+});  
