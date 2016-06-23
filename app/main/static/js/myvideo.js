c03bc44d781b47b650b44ba63b1ad4dce0b9d2ed
xh22 <910192724@qq.com>
2016-06-23 21:32:55 +0800

    fag

---
+++ app/main/static/js/myvideo.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+    jwplayer("container").setup({  
+        flashplayer: "/static/plugin/player.flash.swf",  
+        file: "/static/video/video1.mp4",  
+        height: 622,  
+        width:1104,  
+        stretching : 'fill',  
+        streamer:"start",  
+        provider:"http",  
+    });  
+</script>
