10c854c0d978294b12ef20eedbc79622a4f97015
xh22 <910192724@qq.com2>
2016-07-21 17:47:05 +0800

    fag

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -13,17 +13,19 @@ PERMANENT_SESSION_LIFETIME = timedelta(minutes=60)
 UPLOADED_PHOTOSCERT_DEST = os.path.join(app.root_path, 'static/img/portfolio/0/')
 UPLOADED_PHOTOSPORT_DEST = os.path.join(app.root_path, 'static/img/portfolio/1/')
 UPLOADED_VIDEOS_DEST = os.path.join(app.root_path, 'static/video/')
+UPLOADED_VIDEOPIC_DEST = os.path.join(app.root_path, 'static/img/video/')
 
 ADMIN_EMAIL = 'admin@qq.com'
 
 MAIL_SERVER = "smtp.qq.com"
 MAIL_USERNAME = "910192724" 
-MAIL_PASSWORD = ""
+MAIL_PASSWORD = "kztzbamoukxebbhg"
 MAIL_DEFAULT_SENDER = "910192724@qq.com"
 MAIL_USE_TLS = True
 
 app.config.from_object(__name__)
-app.secret_key = os.urandom(24)
+#app.secret_key = os.urandom(24)
+app.secret_key = "\xd7N\x97\xce\xa0'\xf6\xbb8m\x19\xb5\x15\x97\x8d\xfdk\x98\x9a\x02)i\x0c\xa1"
 
 db = Mysql(app)
 mail = Mail(app)
